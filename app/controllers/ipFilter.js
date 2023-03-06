59999f457137d2424d0a156c82151f642a213862
krisharayanne <krisha.rayanne108@gmail.com>
2023-03-06 10:39:40 +0800

    included ipaddr library in ipFilter middleware

--- app/controllers/ipFilter.js
+++ app/controllers/ipFilter.js
@@ -1,3 +1,4 @@
+const ipaddr = require('ipaddr.js');
 
 // verify public external ipv4 address of client calling this API server
 function verifyClientIP(req, res, next) {
@@ -6,36 +7,44 @@ function verifyClientIP(req, res, next) {
     // currently is my device ip address
    const trustedIps = ['202.186.81.105','13.250.0.107'];
 
-   // start manual IP extraction without ipaddr.js library
-   // declare clientIP
-   let clientIP;
+  //  // start manual IP extraction without ipaddr.js library
+  //  // declare clientIP
+  //  let clientIP;
 
-   let requestIP = req.header('x-forwarded-for');
-   if(requestIP) {
-    clientIP = requestIP.split(',')[0];  
-   }
-   else {
-    let extractedIP = req.headers['x-forwarded-for'] || 
-     req.connection.remoteAddress || 
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress;
-     console.log("Extracted IP: " + extractedIP);
-     clientIP = extractedIP;
+  //  let requestIP = req.header('x-forwarded-for');
+  //  if(requestIP) {
+  //   clientIP = requestIP.split(',')[0];  
+  //  }
+  //  else {
+  //   let extractedIP = req.headers['x-forwarded-for'] || 
+  //    req.connection.remoteAddress || 
+  //    req.socket.remoteAddress ||
+  //    req.connection.socket.remoteAddress;
+  //    console.log("Extracted IP: " + extractedIP);
+  //    clientIP = extractedIP;
 
-     // check if extractedIP is defined or undefined
-     if(extractedIP) {
-      // if true, check if extracted ip is in ipv4 or ipv6 version 
-     // if in ipv6, convert to ipv4,
-     // assign ipv4 address to clientIP variable
-     }
+  //    // check if extractedIP is defined or undefined
+  //    if(extractedIP) {
+  //     // if true, check if extracted ip is in ipv4 or ipv6 version 
+  //    // if in ipv6, convert to ipv4,
+  //    // assign ipv4 address to clientIP variable
+  //    }
        
-   }
-   // end manual IP extraction without ipaddr.js library
+  //  }
+  //  // end manual IP extraction without ipaddr.js library
+
+  // start automatic IP extraction with ipaddr.js library
+  let remoteAddress = req.ip;
+  if (ipaddr.isValid(req.ip)) {
+    remoteAddress = ipaddr.process(req.ip).toString();
+    console.log("remoteAddress using ipaddr library: " + remoteAddress);
+  }
+   // end automatic IP extraction with ipaddr.js library
 
    console.log("IP Filter Middleware!");
-   console.log("requestIP from IP Filter Middleware: " + requestIP);
-   console.log("clientIP from IP Filter Middleware: " + clientIP);
-   if(trustedIps.indexOf(clientIP) >= 0) {
+  //  console.log("requestIP from IP Filter Middleware: " + requestIP);
+  //  console.log("clientIP from IP Filter Middleware: " + clientIP);
+   if(trustedIps.indexOf(remoteAddress) >= 0) {
  
      // do stuff
      console.log("Message sent from trusted ip!");
